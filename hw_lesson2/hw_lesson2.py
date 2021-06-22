420503236c08735869c7fb06f7dd8965607b88d2
FaridPF <faridPF@gmail.com>
2021-06-22 22:52:08 +0300

    Create hw_lesson2.py

---
+++ hw_lesson2/hw_lesson2.py
@@ -0,0 +1,30 @@
+# =========TASK1=========
+from datetime import date
+
+today = date.today()
+first_name = 'Farid'
+second_name = 'Mbaidin'
+
+print(f'Good day {first_name}! {today} is a perfect day to learn some python.')
+
+# =========TASK2=========
+
+full_name = first_name +' '+ second_name
+
+print(f'Your full name is {full_name}')
+
+# =========TASK3=========
+
+a = 3
+b = 5
+
+print(a+b)
+print(b-a)
+print(a*b)
+print(b/a)
+print(a**b)
+print(a%b)
+print(b//a)
+
+
+
