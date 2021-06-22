d37efa2e01a1ee1ae8676783b691264347581dd5
FaridPF <faridPF@gmail.com>
2021-06-22 22:26:13 +0300

    Initial commit for BeetRoot HomeWork

---
+++ hw_lesson1/hw_lesson1.py
@@ -0,0 +1,21 @@
+# =======TASK2=======
+
+# Regular print function
+print('Hi Beetroot\n')
+
+# Spaces betwen words would be interchanged with '****' sequence in the end of the line 'xYxYxYxY' would be added
+print('Hello World','hi all','lollll',sep='****',end='\nxYxYxYxY\n')
+
+
+# Each argument would be printed from the new line
+print('\nI','look', 'like','a', 'poem!', sep='\n',end='\n\n')
+
+# =======TASK3=======
+
+# Printing 'O' with '#' sines
+
+print(('#'* 9),('\n#\t#'*3),'\n#',('#'* 8),'\n\n',sep='')
+
+# Printing 'H' with '#' sines
+
+print(('#\t'*2 +'\n')*2,'#'*9 + '\n',('#\t'*2 +'\n')*2,sep='')
